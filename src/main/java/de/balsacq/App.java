bda5b798ea6ae26450add415b9c8d908ef82bff5
maximbalsacq <maxim.balsacq@hs-furtwangen.de>
2017-11-28 14:20:27 +0100

    initial commit

---
+++ src/main/java/de/balsacq/App.java
@@ -0,0 +1,15 @@
+package de.balsacq;
+
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args ) {
+		Scanner s = new Scanner(System.in);
+		String n = s.next();
+        System.out.println( n.toUpperCase() );
+    }
+}
